76a7f16b682bb38a0a72d14ed616fbb7b805e3a1
Yahya08 <yahyahanda.r@students.amikom.ac.id>
2024-05-28 21:53:42 +0700

    update product

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-
 import { BrowserRouter } from 'react-router-dom';
+import './style/main.css';
 
-import './dist/css/main.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import './style/bootstrap.min.css'; // Mengimpor Bootstrap CSS
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter> 
-    <App />
-
-
-
+      <App />
     </BrowserRouter>
   </React.StrictMode>,
 )
