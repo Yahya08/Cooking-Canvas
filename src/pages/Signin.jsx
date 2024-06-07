e582fce86a7212f576dbcc91ee30e58c700aff15
Hamim21114395 <hamimnurkhamid@students.amikom.ac.id>
2024-06-07 20:31:02 +0700

    .

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -18,27 +18,27 @@ const Signin = () => {
         >
           {/* Konten opsional di sebelah kiri */}
         </Col>
-        <Col md={6} className="d-flex flex-column align-items-center justify-content-center">
+        <Col md={6} className="d-flex flex-column align-items-center justify-content-center" style={{ border: '1px solid #ced4da', borderRadius: '5px', padding: '20px' }}>
           <div className="text-center mb-4 d-flex align-items-center">
-            <img src="logo2.png" alt="Logo" style={{ width: '200px', marginBottom: '10px' }} />
+            <img src="logo3.svg" alt="Logo" style={{ width: '200px', marginBottom: '10px' }} />
           </div>
-          <div className="mb-4" style={{ textAlign: 'left', width: '100%', maxWidth: '400px' }}>
-            <h4 style={{ marginBottom: '10px', color: '#0F172A', fontWeight:'bold'  }}>Sign in to your account</h4>
+          <div className="mb-4" style={{ textAlign: 'left', width: '100%', maxWidth: '400px', padding: '20px', border: '1px solid #ced4da', borderRadius: '5px' }}>
+            <h4 style={{ marginBottom: '10px', color: '#0F172A', fontWeight:'bold' }}>Sign in to your account</h4>
             <p style={{ marginBottom: '20px', color: '#6c757d' }}>Enter your email below</p>
+            <Form style={{ width: '100%' }}>
+              <Form.Group controlId="email">
+                <Form.Label style={{ color: '#0F172A', textAlign: 'left', width: '100%' }}>Email</Form.Label>
+                <Form.Control type="email" placeholder="" defaultValue="m@example.com" />
+              </Form.Group>
+              <Form.Group controlId="password" style={{ marginTop: '20px' }}>
+                <Form.Label style={{ color: '#0F172A', textAlign: 'left', width: '100%' }}>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+              </Form.Group>
+              <Button variant="dark" type="submit" style={{ marginTop: '20px', width: '100%' }}>
+                Sign In
+              </Button>
+            </Form>
           </div>
-          <Form style={{ width: '100%', maxWidth: '400px' }}>
-            <Form.Group controlId="email">
-              <Form.Label style={{ color: '#0F172A', textAlign: 'left', width: '100%' }}>Email</Form.Label>
-              <Form.Control type="email" placeholder="" defaultValue="m@example.com" />
-            </Form.Group>
-            <Form.Group controlId="password" style={{ marginTop: '20px' }}>
-              <Form.Label style={{ color: '#0F172A', textAlign: 'left', width: '100%' }}>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
-            </Form.Group>
-            <Button variant="dark" type="submit" style={{ marginTop: '20px', width: '100%' }}>
-              Sign In
-            </Button>
-          </Form>
           <p style={{ marginTop: '20px' }}>Don't have an account yet? <a href="#" style={{ color: 'inherit', fontWeight: 'bold' }}>Sign up</a></p>
         </Col>
       </Row>
